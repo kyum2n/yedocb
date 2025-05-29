2e242da5bfcd079b4b530d0bb77f8a3f04a89dc2
kwangwon <jkw4563@gmail.com>
2025-05-29 16:29:03 +0900

    jkw 25.05.29 admin3

--- src/main/java/com/example/yedocb/user/repository/UserMapper.java
+++ src/main/java/com/example/yedocb/user/repository/UserMapper.java
@@ -3,11 +3,23 @@ package com.example.yedocb.user.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.yedocb.user.entity.User;
 
 @Mapper
 public interface UserMapper {
+	List<User> selectAllUsers();
+	
+	// 로그인용 ID, 비밀번호 확인
+    User selectByUIdAndPwd(@Param("uId") String uId, @Param("uPwd") String uPwd);
+    
+    // 전화번호 또는 비밀번호 수정
+    int updateUserPhonePwd(@Param("uId") String uId,
+            @Param("phone") String phone,
+            @Param("pwd") String pwd);
+    
+    // 회원 가입
     int insertUser(User user);
     User selectByUId(String uId);
     User selectByUEmail(String uEmail);
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
-INSERT INTO admin (aid, aemail, apwd, role, created_by)
+INSERT INTO admin (aid, aemail, apwd, role, createdBy)
 VALUES ('super', 'superadmin@naver.com', 'super1234', 'super', NULL);
+
+INSERT INTO users (uid, uname, uemail, upwd, uphone)
+VALUES ('test', '테스트유저', 'test@example.com', '1234', '010-0000-0000');
+
