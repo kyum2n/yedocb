13f7a3d6b96340a33cb5c2f7dee3f00e591b9008
yonsai <aphrod207@naver.com>
2025-06-20 09:18:05 +0900

    lkm 25.06.20 schema2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -14,5 +14,12 @@ VALUES ('test', '테스트유저', 'test@example.com', '$2a$10$tTrBmsF8CaOmimgOn
 INSERT INTO reservations (uId, tName, consultDate, consultTime, status)
 VALUES ('test', '보톡스 시술', '2025-06-01', '14:30:00', '대기');
 
+INSERT INTO noticeEvent (
+    netitle, necontent, neimageurl, netype, nestartdate, neenddate, necreatedat, neupdatedat
+) VALUES
+('이벤트1', '이벤트입니다', 'https://res.cloudinary.com/dml7imecd/image/upload/v1749713047/eventPopup3_qu7fok.png', '이벤트', NULL, NULL, '2025-06-12 17:44:07.312307', '2025-06-12 17:44:07.312307'),
+('공지사항1', '공지사항입니다', 'https://res.cloudinary.com/dml7imecd/image/upload/v1749713067/notise_dshlec.png', '공지사항', NULL, NULL, '2025-06-12 17:44:07.312307', '2025-06-12 17:44:07.312307');
+
+
 INSERT INTO inquiry (uId, uName, uEmail, visit, qContent, createdAt, qStatus)
 VALUES ('test', '테스트유저', 'test@example.com', true, '시술 전 주의사항에 대해 궁금합니다.', '2025-06-01 00:00:00', '답변 대기');
